367977f733a4f365b096fae00e450dc1ca83053a
Teksiak <bednarowski.wojciech@gmail.com>
2024-09-06 19:45:22 +0200

    fix: Change ripple effect on the navigation tabs.

--- core/presentation/src/main/java/com/teksiak/nutrilight/core/presentation/designsystem/components/NutrilightNavBar.kt
+++ core/presentation/src/main/java/com/teksiak/nutrilight/core/presentation/designsystem/components/NutrilightNavBar.kt
@@ -2,6 +2,7 @@ package com.teksiak.nutrilight.core.presentation.designsystem.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,7 +12,9 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -19,6 +22,7 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.teksiak.nutrilight.core.presentation.NavTab
+import com.teksiak.nutrilight.core.presentation.designsystem.AlmostWhite
 import com.teksiak.nutrilight.core.presentation.designsystem.Primary
 import com.teksiak.nutrilight.core.presentation.designsystem.ShadedWhite
 import com.teksiak.nutrilight.core.presentation.designsystem.Silver
@@ -37,7 +41,7 @@ fun NutrilightNavBar(
             .fillMaxWidth()
             .background(White)
             .topBorder(1.dp, ShadedWhite)
-            .padding(horizontal = 16.dp, vertical = 8.dp),
+            .padding(horizontal = 16.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -58,12 +62,22 @@ private fun NavigationTab(
     isSelected: Boolean = false,
     onClick: () -> Unit = {}
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
+
     Column(
         modifier = modifier
             .width(60.dp)
-            .clickable {
+            .clickable(
+                interactionSource = interactionSource,
+                indication = ripple(
+                    color = Silver,
+                    bounded = true,
+                    radius = 30.dp
+                )
+            ) {
                 onClick()
-            },
+            }
+            .padding(vertical = 8.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(2.dp)
     ) {
